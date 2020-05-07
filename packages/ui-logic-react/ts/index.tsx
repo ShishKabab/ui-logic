2be565e7796e3d0e5ed05cfeb306394966756a07
Vincent den Boer <vincent@shishkabab.net>
2020-05-07 13:38:33 +0200

    Solve bug reading from this.state right after calling this.setState() in React

--- packages/ui-logic-react/ts/index.tsx
+++ packages/ui-logic-react/ts/index.tsx
@@ -1,48 +1,65 @@
-import { Component } from 'react';
+import { Component } from 'react'
 import * as logic from 'ui-logic-core'
 
-export abstract class UIElement<Props, State = {}, Event extends logic.UIEvent<{}> = logic.UIEvent<{}> > extends Component<Props, State> {
+export abstract class UIElement<
+    Props,
+    State = {},
+    Event extends logic.UIEvent<{}> = logic.UIEvent<{}>
+> extends Component<Props, State> {
     public logic?: logic.UILogic<State, Event>
     // private baseEventHandlers = new EventHandlers()
+    private newestState?: State
 
-    constructor(props : Props, options : { logic?: logic.UILogic<State, Event> } = {}) {
+    constructor(
+        props: Props,
+        options: { logic?: logic.UILogic<State, Event> } = {},
+    ) {
         super(props)
 
         this.logic = options.logic
         if (this.logic) {
             const initialState = this.logic.getInitialState()
             if (initialState) {
-                (this as any).state = this.logic.getInitialState()
+                ;(this as any).state = this.newestState = this.logic.getInitialState()
             }
         }
     }
-    
-    async processEvent<EventName extends keyof Event>(eventName : EventName, event : Event[EventName], options? : { optional : boolean }) {
+
+    async processEvent<EventName extends keyof Event>(
+        eventName: EventName,
+        event: Event[EventName],
+        options?: { optional: boolean },
+    ) {
         if (!this.logic) {
             throw new Error('Tried to process event in UIElement without logic')
         }
-        
+
         const mutation = await this.logic.processUIEvent(eventName, {
-            previousState: this.state,
+            previousState: this.newestState || this.state,
             event,
             direct: true,
-            optional: options && options.optional
+            optional: options && options.optional,
         })
         if (mutation) {
             this.processMutation(mutation)
         }
     }
 
-    processMutation(mutation : logic.UIMutation<State>) {
+    processMutation(mutation: logic.UIMutation<State>) {
         if (this.logic) {
-            const newState = this.logic.withMutation(this.state, mutation as any)
-            this.setState(newState)
+            this.newestState = this.logic.withMutation(
+                this.newestState || this.state,
+                mutation as any,
+            )
+            this.setState(this.newestState)
         }
     }
 
     componentWillMount() {
         if (this.logic) {
-            this.logic.events.addListener('mutation', mutation => this.processMutation(mutation))
+            this.logic.events.addListener('mutation', (mutation) =>
+                this.processMutation(mutation),
+            )
         }
     }
 
